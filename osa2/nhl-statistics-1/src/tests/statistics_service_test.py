a5e6bf279b88f1f5bd8821317cae9a81a06d0ea5
juvekole <jussi.v.kolehmainen@student.jyu.fi>
2025-11-06 22:08:09 +0200

    statistics services testikattavuus 100%. Omit itse testit

--- osa2/nhl-statistics-1/src/tests/statistics_service_test.py
+++ osa2/nhl-statistics-1/src/tests/statistics_service_test.py
@@ -36,14 +36,11 @@ class TestStatisticService(unittest.TestCase):
         1. Varmistaa riippuvuuden injektoinnin (self._reader).
         2. Varmistaa datan lataamisen (self._players).
         """
-
-        # Tarkista, että lukija on tallennettu (assertIsInstance toimii nyt)
+        # Tarkista, että lukija on tallennettu 
         self.assertIsInstance(self.stats._reader, PlayerReaderStub)
 
-        # Tarkista, että pelaajat on ladattu
-        players_list = self.stats._players
-
         # Varmista, että pelaajia on 5 (Määritelty PlayerReaderStub:issa)
+        players_list = self.stats._players
         self.assertEqual(len(players_list), 5)
 
         # Varmista, että listan ensimmäinen pelaaja on se, mitä odotetaan
@@ -51,4 +48,42 @@ class TestStatisticService(unittest.TestCase):
         self.assertEqual(players_list[0].team, "EDM")
         self.assertEqual(players_list[0].goals, 4)
         self.assertEqual(players_list[0].assists, 12)
- 
+
+    def test_etsi_onnistuu(self):
+        "find metodi onnistuu"
+        player = self.stats.search("Lemieux")
+        self.assertEqual(player.name, "Lemieux")
+        self.assertEqual(player.team, "PIT")
+
+    def test_etsi_ei_onnistu(self):
+        "find metodi ei onnistu"
+        player = self.stats.search("eiole")
+        self.assertIsNone(player)
+
+    def test_listaa_joukkueen_pelaajat(self):
+        "team metodi testaus: listaa joukkueen pelaajat"
+        # varmistetaan että EDM joukkueesta löytyy 3 pelaajaa.
+        # team metod on statistics servicen etsijä metodi
+        edm_players = self.stats.team("EDM")
+        self.assertEqual(len(edm_players), 3)
+        # tarlostetaan ensimmäienn ja viimeinen pelaaja
+        self.assertEqual(edm_players[0].name, "Semenko")
+        self.assertEqual(edm_players[2].name, "Gretzky")
+        # varmistetaan myös että kaikki pelaajat ovat EDM:stä
+        for player in edm_players:
+            self.assertEqual(player.team, "EDM")
+
+    def test_pelaaja_piste_listaus(self):
+        """
+        top-metodi testi.
+        """
+        top_3 = self.stats.top(2)
+
+        # Varmistetaan, että palautuslista on oikean kokoinen
+        self.assertEqual(len(top_3), 3)
+
+        # Varmistetaan, että ensimmäinen on Gretzky (124 pistettä)
+        self.assertEqual(top_3[0].name, "Gretzky") 
+
+        # Varmistetaan, että kolmas on Yzerman (98 pistettä)
+        self.assertEqual(top_3[2].name, "Yzerman")
