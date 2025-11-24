3eacc23c0542045e6bec135c38f49267dc643fd9
juvekole <jussi.v.kolehmainen@student.jyu.fi>
2025-11-24 20:42:23 +0200

    teht2 osa4.

--- osa4/maksukortti-mock/src/tests/kassapaate_test.py
+++ osa4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -23,3 +23,21 @@ class TestKassapaate(unittest.TestCase):
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()
+
+    def test_kassapaate_lataa_positiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 0
+        ladattava_summa = 15
+
+        self.kassa.lataa(maksukortti_mock, ladattava_summa)
+
+        maksukortti_mock.lataa.assert_called_with(ladattava_summa)
+
+    def test_kassapaate_lataa_negatiivinen(self):
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 0
+        ladattava_summa = -100
+
+        self.kassa.lataa(maksukortti_mock, ladattava_summa)
+
+        maksukortti_mock.lataa.assert_not_called()
